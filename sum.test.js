0fc6bd5948089ba0380b2c047fc368c46919f12a
AntonioMM8506 <antonio.manilla.maldonado@gmail.com>
2024-02-26 11:08:49 -0600

    Download Artifact

--- sum.test.js
+++ sum.test.js
@@ -1,8 +1,10 @@
 const sum =  require("./sum");
 
+/*
 test("adds 1 + 2 is not equal to 4", () => {
     expect(sum(1,2)).toBe(4);
 });
+*/
 
 test("adds 1 + 2 to equal 3", () => {
     expect(sum(1,2)).toBe(3);
