0cc8169e5e123bfa009914183c48dd21dfafce2d
AntonioMM8506 <antonio.manilla.maldonado@gmail.com>
2024-03-04 11:23:41 -0600

    Test failing

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -4,6 +4,8 @@ const github = require("@actions/github");
 // core is used to interact directly with the yaml file
 // When interacting with a yaml file using core, a good practice is to always use try-catch for the execution
 try {
+    throw new Error("Some Error Message");
+
     core.debug("Debug Message");
     core.warning("Warning message");
     core.error("Error message");
@@ -20,6 +22,7 @@ try {
     core.startGroup("Logging github context");
     console.log(JSON.stringify(github.context, null, 2));
     core.endGroup();
+    
     } catch (error) {
     core.setFailed(error.message);
 }
